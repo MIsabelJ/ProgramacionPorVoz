694da784d7ff7c0f73fd47a75f654b18d12883c3
AgusMandolesi <agustin.mandolesi@alumnos.frm.utn.edu.ar>
2023-05-17 20:32:31 -0300

    actualización del gitignore

--- textProcessing/services/create/createBranch.py
+++ textProcessing/services/create/createBranch.py
@@ -18,4 +18,36 @@ def crear(palabraDividida):
         "imprimir"
     ]
 
-    
+    for palabra in palabraDividida:
+        if palabra == "variable":
+            palabraDividida.remove("variable")
+            print("Se ha creado una variable")
+            break
+        if palabra in ["funcion", "metodo"]:
+            palabraDividida.remove(palabra)
+            print("Se ha creado una funcion")
+            break
+        if palabra in ["lista", "arreglo", "array"]:
+            palabraDividida.remove(palabra)
+            print("Se ha creado una lista")
+            break
+        if palabra in ["for","para"]:
+            palabraDividida.remove(palabra)
+            print("Se ha creado un for")
+            break
+        if palabra in ["while", "mientras"]:
+            palabraDividida.remove(palabra)
+            print("Se ha creado un while")
+            break
+        if palabra in ["condicional", "if"]:
+            palabraDividida.remove(palabra)
+            print("Se ha creado un if")
+            break
+        if palabra == "comentario":
+            palabraDividida.remove(palabra)
+            print("Se ha creado un comentario")
+            break
+        if palabra in ["mensaje", "print", "imprimir"]:
+            palabraDividida.remove(palabra)
+            print("Se ha creado un mensaje")
+            break
