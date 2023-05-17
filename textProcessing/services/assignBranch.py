215d00f0677f33247c3378c4435b044c935ef6b8
AgusMandolesi <agustin.mandolesi@alumnos.frm.utn.edu.ar>
2023-05-17 19:55:26 -0300

    agregada funcionalidad a la rama de asignación de variables

--- textProcessing/services/assignBranch.py
+++ textProcessing/services/assignBranch.py
@@ -1,32 +1,42 @@
-def asignar(palabraDividida, arrayNombres):
+def asignar(instruccion, arrayNombres):
     nombre = ""
     valor = ""
 
+    #Palabras claves que anteceden a la variable buscada
     indicadoresNombre = ["nombre", "llamada", "variable", "a", "es"]
     indicadoresValor = [
         "valor",
-        "de",
         "numero",
         "string",
         "cadena",
         "texto",
-        "palabra",
-        "palabras",
+        "instruccion",
+        "instrucciones",
         "entero",
     ]
 
-    nombre = filtrar(indicadoresNombre, palabraDividida)
+    #Se encuentra el valor a asignar y se eliminan esas palabras de la instrucción
+    #para que no interfieran con el análisis del nombre de la variable
+    posicionValor = filtrar(indicadoresValor, instruccion)[1]
+    for i in range(len(instruccion)):
+        if i >= posicionValor:
+            valor += instruccion[i] + " "
+            instruccion[i] = ""
+
+    #Se encuentra el nombre de la variable
+    nombre = filtrar(indicadoresNombre, instruccion)[0]
     if nombre in arrayNombres:
-        valor = filtrar(indicadoresValor, palabraDividida)
         print(nombre, " = ", valor)
     else:
         print(f"La variable {nombre} no existe")
- 
 
-def filtrar(indicadores, palabraDividida):
-    for i in reversed(range(len(palabraDividida))):
-        if palabraDividida[i] in indicadores:
-            if i > 0 and palabraDividida[i-1] in indicadores:
-                return palabraDividida[i]
+#filtra la instrucción para encontrar la palabra clave que antecede al valor esperado.
+#Devuelve una tupla con la palabra clave y su posición en la instrucción.
+def filtrar(indicadores, instruccion):
+    for i in reversed(range(len(instruccion))):
+        if instruccion[i] in indicadores:
+            if i > 0 and instruccion[i-1] in indicadores and instruccion[i-2] in indicadores:
+                return (instruccion[i], i)
             else:
-                return palabraDividida[i+1]  # Añadir +2 para obtener el valor en lugar del índice i+1
+                return (instruccion[i+1], (i+1))
+    
--- textProcessing/textProcessing.py
+++ textProcessing/textProcessing.py
@@ -12,12 +12,13 @@ insertar = Node("insertar")
 nodosPadre = [crear, llamar, eliminar, asignar, comentar, mover, insertar]
 arrayNombres = ["numero", "perro", "edad", "nombre", "cantidad", "llamada"]
 
+ejemplo = "asignar a la variable cuyo nombre es nombre el valor metodología de la investigación es piola"
 # ejemplo = "asignar a la variable numero el valor 8"
-ejemplo = "asignar a la variable cuyo nombre es perro el valor agus"
-# ejemplo = "asignar a la variable llamada número el valor 8"
-# ejemplo = "asignar a la variable cuyo nombre es número el valor 8"
-# ejemplo = "asignar a número el valor 8"
-# ejemplo = "asignar a la variable de nombre número el valor 8"
+# ejemplo = "asignar a la variable llamada nombre el valor 8"
+# ejemplo = "asignar a la variable cuyo nombre es numero el valor 8"
+# ejemplo = "asignar a numero el valor 8"
+# ejemplo = "asignar a la variable de nombre numero el valor 8"
 
 palabraDividida = ejemplo.split(" ")
 index.abrirNodo(palabraDividida, arrayNombres)
+
