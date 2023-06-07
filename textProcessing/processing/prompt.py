02d11a351edb83c07689cbee51e20170ba7cb2e6
GabrielGiorgis <gabi.giorgis6@gmail.com>
2023-06-07 19:26:26 -0300

    Modificacion-Prompt

--- textProcessing/processing/prompt.py
+++ textProcessing/processing/prompt.py
@@ -1,9 +1,9 @@
 import openai
 def traducir(instruccion):
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     model_engine = "text-davinci-002"
-    prompt = f"En python y sin agregados {instruccion}"
+    prompt = f"En python y sin agregados. Si lo siguiente no es algo relacionado con codigo dame como respuesta un mensaje que diga 'Error, no es relacionado con codigo': {instruccion}"
     completion = openai.Completion.create(engine=model_engine,
                                           prompt=prompt,
                                           max_tokens=1024,
